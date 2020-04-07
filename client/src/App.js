68909e8a2fd569132089c578f2692f69e0e7d5fb
Godstime <lewisagho47@gmail.com>
2020-04-08 00:55:41 +0100

    make it better

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,6 @@ class App extends Component {
     return (
       <Router>
         <NavBar />
-
         <Route exact path="/" component={Landing} />
         <Route exact path="/sign-up" component={Auth} />
         <Route exact path="/dashboard" component={ChattRoom} />
